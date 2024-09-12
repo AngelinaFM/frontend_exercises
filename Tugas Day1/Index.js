3a118ed73a6461afbb5b999bd7eb51a05f296994
AngelinaFM <angelinafebriani3@gmail.com>
2024-09-12 11:03:10 +0800

    Tugas 1 (Day 1)
    .
    bfi;hdsch d

---
+++ Tugas Day1/Index.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+let nama = "Angelina Febriani Madesen"; 
+let umur = 19; 
+let ApakahMahasiswa= true; 
+
+console.log(nama);
+console.log(umur);
+console.log(ApakahMahasiswa);
+
+
